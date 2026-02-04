f1eeff03200d8c31df05a5e377a467eda49a622b
Tanisha-221 <tanishajindal223@gmail.com>
2026-02-04 19:29:18 +0530

    modification

--- tf_Config/main.tf
+++ tf_Config/main.tf
@@ -4,7 +4,7 @@ locals {
 
 resource "azurerm_resource_group" "example" {
   name     = "${var.prefix}-resourceGroup"
-  location = "Central India"
+  location = "West Europe"
   tags = {
     Environment = "Sandbox"
     CreatedOn   = local.current_date
